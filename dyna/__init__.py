5dac88077d2b99ef0545e1426cdb6ef0a02decd3
Daniel Jones <daniel@jones.org.uk>
2021-04-30 22:29:26 +0100

    Basic components and volume/panning listeners

--- dyna/__init__.py
+++ dyna/__init__.py
@@ -1,3 +1,47 @@
+import Live
+import inspect
+import logging
+logger = logging.getLogger("liveosc")
+
+def describe_module(module):
+    """
+    Describe the module object passed as argument
+    including its root classes and functions """
+
+    logger.info("Module: %s" % module)
+    for name in dir(module):
+        obj = getattr(module, name)
+        if inspect.ismodule(obj):
+            describe_module(obj)
+        elif inspect.isclass(obj):
+            logger.info("Class: %s" % name)
+            members = inspect.getmembers(obj)
+
+            logger.info("Builtins")
+            for name, member in members:
+                if inspect.isbuiltin(member):
+                    logger.info(" - %s" % (name))
+
+            logger.info("Functions")
+            for name, member in members:
+                if inspect.isfunction(member):
+                    logger.info(" - %s" % (name))
+
+            logger.info("Properties")
+            for name, member in members:
+                if str(type(member)) == "<type 'property'>":
+                    logger.info(" - %s" % (name))
+
+            logger.info("----------")
+
+    for name in dir(module):
+        obj = getattr(module, name)
+        if (inspect.ismethod(obj) or inspect.isfunction(obj)):
+            logger.info("Method", obj)
+
 class DynamicClass:
     def foo(self):
-        return "baz"
+        return "bar"
+
+    def describe_live(self):
+        describe_module(Live)
--- tester.py
+++ tester.py
@@ -1,19 +1,21 @@
-from ableton.v2.control_surface import ControlSurface, Component
+from ableton.v2.control_surface import ControlSurface
+from ableton.v2.control_surface.components import TransportComponent, MixerComponent, ChannelStripComponent, SessionRingComponent, SessionComponent
 from ableton.v2.base.event import listens
+from ableton.v2.base import liveobj_valid
 
 from . import dyna
 import importlib
 import traceback
 import logging
 
-logger = logging.getLogger()
+logger = logging.getLogger("liveosc")
 file_handler = logging.FileHandler('/tmp/liveosc.log')
 file_handler.setLevel(logging.INFO)
 formatter = logging.Formatter('(%(asctime)s) [%(levelname)s] %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
-class TransportState(Component):
+class CustomTransportComponent (TransportComponent):
     def __init__(self, manager):
         super().__init__()
         self.manager = manager
@@ -22,12 +24,50 @@ class TransportState(Component):
 
     @listens('tempo')
     def _on_tempo_changed(self):
-        logging.info("Tempo changed (%.1f)" % self.song.tempo)
+        logger.info("Tempo changed (%.1f)" % self.song.tempo)
 
     @listens('is_playing')
     def _on_playing_changed(self):
         if self.song.is_playing:
             self.manager.reload()
+
+class CustomMixerComponent (MixerComponent):
+    def __init__(self, manager, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.manager = manager
+
+class CustomChannelStripComponent (ChannelStripComponent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        logger.info("Instantiated ChannelStripComponent")
+
+    def set_track(self, track):
+        super().set_track(track)
+        logger.info(" - Set track %s" % track)
+        if liveobj_valid(track):
+            self._on_playing_slot_index_changed.subject = track
+            self._on_volume_changed.subject = track.mixer_device.volume
+            self._on_panning_changed.subject = track.mixer_device.panning
+        else:
+            logger.info(" - Invalid")
+            self._on_playing_slot_index_changed.subject = None
+            self._on_volume_changed.subject = None
+            self._on_panning_changed.subject = None
+
+    @listens('value')
+    def _on_volume_changed(self):
+        logger.info("Volume changed: %s" % self._track.mixer_device.volume.value)
+
+    @listens('value')
+    def _on_panning_changed(self):
+        logger.info("Panning changed: %s" % self._track.mixer_device.panning.value)
+
+    @listens('playing_slot_index')
+    def _on_playing_slot_index_changed(self):
+        logger.info("Slot index changed: %s" % self._track.playing_slot_index)
+
+class CustomSessionComponent (SessionComponent):
+    pass
 
 class Tester (ControlSurface):
     def __init__(self, c_instance):
@@ -38,7 +78,12 @@ class Tester (ControlSurface):
         # Needed when first registering components
         #--------------------------------------------------------------------------------
         with self.component_guard():
-            self.transport_state = TransportState(self)
+            self.transport = CustomTransportComponent(self)
+            self.session_ring = SessionRingComponent()
+            self.session = CustomSessionComponent(session_ring=self.session_ring)
+            self.mixer = CustomMixerComponent(self,
+                                              tracks_provider=self.session_ring,
+                                              channel_strip_component_type=CustomChannelStripComponent)
 
     def reload(self):
         try:

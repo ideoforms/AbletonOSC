f588f3ecc33750e659cc80f187347fda247ce7e9
Daniel Jones <daniel@jones.org.uk>
2021-05-01 15:17:22 +0100

    OSC tidyup

--- dyna/osc_server.py
+++ dyna/osc_server.py
@@ -1,43 +1,49 @@
 from . import OSC
+from typing import Tuple, Any
+
 import errno
 import socket
 import traceback
 
 import logging
 
-class OSCHandler:
+class OSCServer:
     def __init__(self, local_addr=('127.0.0.1', 9000), remote_addr=('127.0.0.1', 9001)):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self._socket.setblocking(0)
-
         self._local_addr = local_addr
         self._remote_addr = remote_addr
 
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.setblocking(0)
         self._socket.bind(self._local_addr)
-        self.logger = logging.getLogger("liveosc")
 
-        self.logger.info('Starting on: ' + str(self._local_addr) + ', remote addr: ' + str(self._remote_addr))
+        self.logger = logging.getLogger("liveosc")
+        self.logger.info("Starting OSC server (local %s, remote %s)",
+                         str(self._local_addr), str(self._remote_addr))
 
         self._callback_manager = OSC.CallbackManager()
         self._callback_manager.add("/test", lambda msg, source: self.logger.info("Received test!"))
 
-    def send(self, address, msg):
-        oscmsg = OSC.OSCMessage(address, msg)
+    def send(self, address: str, params: Tuple[Any] = ()) -> None:
+        """
+        Args:
+            address: The OSC address (e.g. /frequency)
+            params: A tuple of zero or more OSC params
+        """
+        oscmsg = OSC.OSCMessage(address, params)
         if len(oscmsg.error) > 0:
-            self.logger.info('OSCMessage Error: ' + ''.join(oscmsg.error))
+            self.logger.info("Error sending OSC message: %s" % oscmsg.error)
 
         self._socket.sendto(oscmsg.getBinary(), self._remote_addr)
 
-    def send_message(self, message):
-        self._socket.sendto(message.getBinary(), self._remote_addr)
-
-    def process(self):
+    def process(self) -> None:
+        """
+        Synchronously process all data queued on the OSC socket.
+        """
         try:
             while 1:
-                self._data, self._addr = self._socket.recvfrom(65536)
-                self.logger.info("Received %d" % len(self._data))
+                data, addr = self._socket.recvfrom(65536)
                 try:
-                    self._callback_manager.handle(self._data, self._addr)
+                    self._callback_manager.handle(data, addr)
 
                 except OSC.NoSuchCallback as e:
                     errmsg = 'Unknown callback: ' + str(e.args[0])
@@ -46,7 +52,7 @@ class OSCHandler:
                 except Exception as e:
                     errmsg = type(e).__name__ + ': ' + str(e.args[0])
 
-                    self.logger.info('LiveOSC: error handling message ' + errmsg)
+                    self.logger.info('LiveOSC: Error handling message: ' + errmsg)
                     self.logger.info("".join(traceback.format_exc()))
 
         except socket.error as e:
@@ -58,5 +64,8 @@ class OSCHandler:
         except Exception as e:
             self.logger.warning('LiveOSC: error handling message ' + type(e).__name__ + ':' + str(e.args[0]))
 
-    def shutdown(self):
+    def shutdown(self) -> None:
+        """
+        Shutdown the server network sockets.
+        """
         self._socket.close()
--- manager.py
+++ manager.py
@@ -21,8 +21,8 @@ class Manager (ControlSurface):
         self.show_message("Loaded LiveOSC")
         self.create_session()
 
-        self.osc_handler = dyna.OSCHandler()
-        self.schedule_message(1, self.refresh)
+        self.osc_server = dyna.OSCServer()
+        self.schedule_message(0, self.tick)
 
     def create_session(self):
         #--------------------------------------------------------------------------------
@@ -36,10 +36,21 @@ class Manager (ControlSurface):
                                                    tracks_provider=self.session_ring,
                                                    channel_strip_component_type=dyna.CustomChannelStripComponent)
 
-    def refresh(self):
-        logger.debug("Refresh...")
-        self.osc_handler.process()
-        self.schedule_message(1, self.refresh)
+        self.song.add_tempo_listener(self.on_tempo_changed)
+
+    def on_tempo_changed(self):
+        self.show_message("Tempo: %.1f" % self.song.tempo)
+
+    def tick(self):
+        """
+        Called once per 100ms "tick".
+        Live's embedded Python implementation does not appear to support threading,
+        and beachballs when a thread is started. Instead, this approach allows long-running
+        processes such as the OSC server to perform operations.
+        """
+        logger.info("Tick...")
+        self.osc_server.process()
+        self.schedule_message(1, self.tick)
 
     def reload_imports(self):
         try:
@@ -51,5 +62,5 @@ class Manager (ControlSurface):
 
     def disconnect(self):
         self.show_message("Disconnecting...")
-        self.osc_handler.shutdown()
+        self.osc_server.shutdown()
         super().disconnect()

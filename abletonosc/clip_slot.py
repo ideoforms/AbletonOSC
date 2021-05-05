2d56a92c7e44307e5a6d92c1be357ad06d4987fe
Daniel Jones <daniel@jones.org.uk>
2021-05-05 22:38:04 +0100

    Add clip_slot properties

--- abletonosc/clip_slot.py
+++ abletonosc/clip_slot.py
@@ -14,8 +14,40 @@ class ClipSlotComponent(AbletonOSCComponent):
                 track_index, clip_index = params[:2]
                 track = self.song.tracks[track_index]
                 clip_slot = track.clip_slots[clip_index]
-                return func(clip_slot, *args)
+                return func(clip_slot, *args, params[2:])
+
             return clip_slot_callback
 
-        for method in ["fire", "stop"]:
-            self.osc_server.add_handler("/live/clip_slot/%s" % method, create_clip_slot_callback(self._call_method, method))
+        methods = [
+            "fire",
+            "stop",
+            "create_clip",
+            "delete_clip"
+        ]
+        properties_r = [
+            "has_clip",
+            "controls_other_clips",
+            "is_group_slot",
+            "is_playing",
+            "is_triggered",
+            "playing_status",
+            "will_record_on_start",
+        ]
+        properties_rw = [
+            "has_stop_button"
+        ]
+
+        for method in methods:
+            self.osc_server.add_handler("/live/clip_slot/%s" % method,
+                                        create_clip_slot_callback(self._call_method, method))
+
+        for prop in properties_r + properties_rw:
+            self.osc_server.add_handler("/live/clip_slot/get_property/%s" % prop,
+                                        create_clip_slot_callback(self._get_property, prop))
+            self.osc_server.add_handler("/live/clip_slot/start_property_listen/%s" % prop,
+                                        create_clip_slot_callback(self._start_property_listen, prop))
+            self.osc_server.add_handler("/live/clip_slot/stop_property_listen/%s" % prop,
+                                        create_clip_slot_callback(self._stop_property_listen, prop))
+        for prop in properties_rw:
+            self.osc_server.add_handler("/live/clip_slot/set_property/%s" % prop,
+                                        create_clip_slot_callback(self._set_property, prop))

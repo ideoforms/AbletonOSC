6ae575b0080afd9b544f95692141f075011d1843
Daniel Jones <daniel@jones.org.uk>
2021-05-06 20:45:03 +0100

    Add Track device queries

--- abletonosc/track.py
+++ abletonosc/track.py
@@ -37,26 +37,62 @@ class TrackComponent(AbletonOSCComponent):
             self.osc_server.add_handler("/live/track/set_property/%s" % prop,
                                         create_track_callback(self._set_property, prop))
 
-        def track_set_volume(track, params: Tuple[Any]):
+        def track_get_volume(track, params: Tuple[Any] = ()):
+            return track.mixer_device.volume.value
+
+        def track_set_volume(track, params: Tuple[Any] = ()):
             track.mixer_device.volume.value = params[0]
-        def track_set_panning(track, params: Tuple[Any]):
+
+        def track_get_panning(track, params: Tuple[Any] = ()):
+            return track.mixer_device.panning.value
+
+        def track_set_panning(track, params: Tuple[Any] = ()):
             track.mixer_device.panning.value = params[0]
-        def track_set_send(track, params: Tuple[Any]):
+
+        def track_get_send(track, params: Tuple[Any] = ()):
+            return track.mixer_device.sends[params[0]]
+
+        def track_set_send(track, params: Tuple[Any] = ()):
             send_id, value = params
             track.mixer_device.sends[send_id].value = value
-        # TODO getters
+
+        # For some reason, volume/panning listeners don't seem to be exposed in the API
+        self.osc_server.add_handler("/live/track/get_property/volume", create_track_callback(track_get_volume))
         self.osc_server.add_handler("/live/track/set_property/volume", create_track_callback(track_set_volume))
+        self.osc_server.add_handler("/live/track/get_property/panning", create_track_callback(track_get_panning))
         self.osc_server.add_handler("/live/track/set_property/panning", create_track_callback(track_set_panning))
+        self.osc_server.add_handler("/live/track/get_property/send", create_track_callback(track_get_send))
         self.osc_server.add_handler("/live/track/set_property/send", create_track_callback(track_set_send))
 
-        def track_get_clips(track, params: Tuple[Any]):
-            clip_slots = track.clip_slots
-            rv = []
-            for clip_slot in clip_slots:
-                clip = clip_slot.clip
-                if clip:
-                    rv.append(clip.name)
-                else:
-                    rv.append(None)
-            return tuple(rv)
-        self.osc_server.add_handler("/live/track/get_property/clips", create_track_callback(track_get_clips))
+        def track_get_clip_names(track, params: Tuple[Any]):
+            return tuple(clip_slot.clip.name if clip_slot.clip else None for clip_slot in track.clip_slots)
+        def track_get_clip_lengths(track, params: Tuple[Any]):
+            return tuple(clip_slot.clip.length if clip_slot.clip else None for clip_slot in track.clip_slots)
+
+        """
+        Returns a list of clip properties, or Nil if clip is empty
+        """
+        self.osc_server.add_handler("/live/track/get_property/clips/name", create_track_callback(track_get_clip_names))
+        self.osc_server.add_handler("/live/track/get_property/clips/length", create_track_callback(track_get_clip_lengths))
+
+        def track_get_num_devices(track, params: Tuple[Any]):
+            return len(track.devices),
+        def track_get_device_names(track, params: Tuple[Any]):
+            return tuple(device.name for device in track.devices)
+        def track_get_device_types(track, params: Tuple[Any]):
+            return tuple(device.type for device in track.devices)
+        def track_get_device_class_names(track, params: Tuple[Any]):
+            return tuple(device.class_name for device in track.devices)
+        def track_get_device_can_have_chains(track, params: Tuple[Any]):
+            return tuple(device.can_have_chains for device in track.devices)
+
+        """
+         - name: the device's human-readable name
+         - type: 0 = audio_effect, 1 = instrument, 2 = midi_effect
+         - class_name: e.g. Operator, Reverb, AuPluginDevice, PluginDevice, InstrumentGroupDevice
+        """
+        self.osc_server.add_handler("/live/track/get_property/num_devices", create_track_callback(track_get_num_devices))
+        self.osc_server.add_handler("/live/track/get_property/devices/name", create_track_callback(track_get_device_names))
+        self.osc_server.add_handler("/live/track/get_property/devices/type", create_track_callback(track_get_device_types))
+        self.osc_server.add_handler("/live/track/get_property/devices/class_name", create_track_callback(track_get_device_class_names))
+        self.osc_server.add_handler("/live/track/get_property/devices/can_have_chains", create_track_callback(track_get_device_can_have_chains))

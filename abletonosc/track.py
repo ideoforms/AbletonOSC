d51344f6238972b0a06a18c06ccdc7548fb4b54d
Marco Buongiorno Nardelli <mbn@unt.edu>
2022-10-07 16:20:51 -0500

    easy fix for get/volume get/panning in track.py

--- abletonosc/track.py
+++ abletonosc/track.py
@@ -39,13 +39,15 @@ class TrackHandler(AbletonOSCHandler):
                                         create_track_callback(self._set, prop))
 
         def track_get_volume(track, params: Tuple[Any] = ()):
-            return track.mixer_device.volume.value
+            params = (track.mixer_device.volume.value,)
+            return params
 
         def track_set_volume(track, params: Tuple[Any] = ()):
             track.mixer_device.volume.value = params[0]
 
         def track_get_panning(track, params: Tuple[Any] = ()):
-            return track.mixer_device.panning.value
+            params = (track.mixer_device.panning.value,)
+            return params
 
         def track_set_panning(track, params: Tuple[Any] = ()):
             track.mixer_device.panning.value = params[0]

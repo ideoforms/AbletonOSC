1caae1e396bc229f295375996cc71c7da7f0da9f
Daniel Jones <daniel@jones.org.uk>
2022-11-15 13:49:36 +0000

    Only log if log path exists

--- manager.py
+++ manager.py
@@ -17,12 +17,14 @@ else:
     # On Windows, put logs in c:\temp
     tmp_dir = "c:\\temp"
 
-log_path = os.path.join(tmp_dir, "abletonosc.log")
-file_handler = logging.FileHandler(log_path)
-file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('(%(asctime)s) [%(levelname)s] %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+if os.path.exists(tmp_dir):
+    # If temp dir exists, create logger
+    log_path = os.path.join(tmp_dir, "abletonosc.log")
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('(%(asctime)s) [%(levelname)s] %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
 class Manager(ControlSurface):
     def __init__(self, c_instance):

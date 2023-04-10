661c1c89ab0d6f45481062ade24282c5c4d8b06b
Mark Marijnissen <markmarijnissen@gmail.com>
2023-04-11 01:08:55 +0200

    Added set and start_listening methods to the view

--- abletonosc/view.py
+++ abletonosc/view.py
@@ -9,6 +9,7 @@ class ViewHandler(AbletonOSCHandler):
 
     def init_api(self):
         def get_selected_scene(params: Optional[Tuple] = ()):
+            self.logger.info("get selected scene!")
             return (list(self.song.scenes).index(self.song.view.selected_scene),)
 
         def get_selected_track(params: Optional[Tuple] = ()):
@@ -17,6 +18,31 @@ class ViewHandler(AbletonOSCHandler):
         def get_selected_clip(params: Optional[Tuple] = ()):
             return (get_selected_track()[0], get_selected_scene()[0])
 
+        def set_selected_scene(params: Optional[Tuple] = ()):
+            index = params[0]
+            if(index >= 0 and index < len(self.song.scenes)):
+                scene = self.song.scenes[index]
+                self.song.view.selected_scene = scene
+
+        def set_selected_track(params: Optional[Tuple] = ()):
+            index = params[0]
+            if(index >= 0 and index < len(self.song.tracks)):
+                track = self.song.tracks[index]
+                self.song.view.selected_track = track
+
+        def set_selected_clip(params: Optional[Tuple] = ()):
+            set_selected_track((params[0],))
+            set_selected_scene((params[1],))
+
         self.osc_server.add_handler("/live/view/get/selected_scene", get_selected_scene)
         self.osc_server.add_handler("/live/view/get/selected_track", get_selected_track)
         self.osc_server.add_handler("/live/view/get/selected_clip", get_selected_clip)
+
+        self.osc_server.add_handler('/live/view/start_listen/selected_scene', partial(self._start_listen, self.song.view, "selected_scene", getter=get_selected_scene))
+        self.osc_server.add_handler('/live/view/start_listen/selected_track', partial(self._start_listen, self.song.view, "selected_track", getter=get_selected_track))
+        self.osc_server.add_handler('/live/view/stop_listen/selected_scene', partial(self._stop_listen, self.song.view, "selected_scene"))
+        self.osc_server.add_handler('/live/view/stop_listen/selected_track', partial(self._stop_listen, self.song.view, "selected_track"))
+
+        self.osc_server.add_handler("/live/view/set/selected_scene", set_selected_scene)
+        self.osc_server.add_handler("/live/view/set/selected_track", set_selected_track)
+        self.osc_server.add_handler("/live/view/set/selected_clip", set_selected_clip)
